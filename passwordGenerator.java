0fa83171308e5338a8d6842624f42a7b5cb4184c
Kishore-Murali <kishoremurali05@gmail.com>
2023-11-30 20:17:53 +0530

    latest commit

---
+++ passwordGenerator.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+public class passwordGenerator {
+    public static void main(String[] args) {
+    String upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String lower="abcdefghijklmnopqrstuvwxyz";
+    String num="0123456789";
+    String specialChars="<>,.?/>]f]+_-) (*&^%$#@!=";
+    String combination=upper+lower+specialChars+num;
+    int len=8;
+    char[] password=new char[len];
+    Random r=new Random();
+    for(int i=0;i<len;i++){
+    password[i]=combination.charAt(r.nextInt(combination.length()));
+    }
+    System.out.println("Generated Password is:"+new String(password));
+    }
+    }
+    
+
